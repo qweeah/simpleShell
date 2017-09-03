509a7d1d10f043bb7ee0b06295332ba1b2a95416
Billy Zha <qweeah@gmail.com>
2017-09-04 01:55:34 +0800

    Code clead && add makefile

--- Hello.C
+++ Hello.C
@@ -1,11 +1,6 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-
-#include <sys/types.h>
-#include <cstdlib>
-#include <unistd.h>
-#include <wait.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "myProcess.h" //TODO
 
@@ -14,14 +9,13 @@ const int INPUT_BUF_SIZE = 1 << 12;
 int main()
 {
   char input_buf[INPUT_BUF_SIZE];
-  //ShowPrompt(); //TODO : uncomment
   while(ShowPrompt())
   {
     GetOneLine(input_buf);
 
-    // enter: continue || exit||EOF end
-    if(!strlen(input_buf)) return 0; //continue;
-    if(input_buf[0] == EOF || !strcmp(input_buf, "exit")) return 0; //break;
+    // enter: continue || exit || EOF
+    if(!strlen(input_buf)) continue;
+    if(input_buf[0] == EOF || !strcmp(input_buf, "exit")) break;
 
     /* Execute command */
     ExecInput(input_buf, -1);
---
+++ Makefile
@@ -0,0 +1,2 @@
+target:
+	g++ -g -std=c++11 -Wall myProcess.c Hello.C myProcess.h -o Bish
